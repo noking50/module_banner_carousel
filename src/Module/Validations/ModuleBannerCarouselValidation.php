765415f7cd2ed8c1947518dd3acbae747283dd1f
noking <noking50@btsdesign.com.tw>
2018-06-22 04:27:09 +0800

    validation

--- src/Module/Validations/ModuleBannerCarouselValidation.php
+++ src/Module/Validations/ModuleBannerCarouselValidation.php
@@ -11,19 +11,26 @@ class ModuleBannerCarouselValidation extends BaseValidation {
         $rules = [
             'button_link' => ['string', 'nullable', 'max:200'],
             'status' => ['integer', 'required', 'in:0,1'],
-            ##
             'lang' => ['array'],
-            'lang.*.name' => ['string', 'required', 'max:100'],
-            'lang.*.title' => ['string', 'nullable', 'max:100'],
-            'lang.*.subtitle' => ['string', 'nullable', 'max:100'],
-            'lang.*.photo' => [new JsonFile(1, 1)],
-            'lang.*.photo_m' => [new JsonFile(1, 1)],
-            'lang.*.button_text' => ['string', 'nullable', 'max:50'],
+        ];
+        $rules_lang = [
+            'name' => ['string', 'required', 'max:100'],
+            'title' => ['string', 'nullable', 'max:100'],
+            'subtitle' => ['string', 'nullable', 'max:100'],
+            'photo' => [new JsonFile(1, 1)],
+            'photo_m' => [new JsonFile(1, 1)],
+            'button_text' => ['string', 'nullable', 'max:50'],
         ];
         $attributes = array_merge(
                 trans('module_required::validation.attributes'),
                 trans('module_banner_carousel::validation.attributes.module_banner_carousel')
                 );
+        foreach($rules_lang as $k => $v){
+            $rules['lang.*.' . $k] = $v;
+            if(isset($attributes[$k])){
+                $attributes['lang.*.' . $k] = $attributes[$k];
+            }
+        }
 
         return $this->validate($rules, $request_data, $attributes);
     }
@@ -33,19 +40,26 @@ class ModuleBannerCarouselValidation extends BaseValidation {
             'id' => ['integer', 'required'],
             'button_link' => ['string', 'nullable', 'max:200'],
             'status' => ['integer', 'required', 'in:0,1'],
-            ##
             'lang' => ['array'],
-            'lang.*.name' => ['string', 'required', 'max:100'],
-            'lang.*.title' => ['string', 'nullable', 'max:100'],
-            'lang.*.subtitle' => ['string', 'nullable', 'max:100'],
-            'lang.*.photo' => [new JsonFile(1, 1)],
-            'lang.*.photo_m' => [new JsonFile(1, 1)],
-            'lang.*.button_text' => ['string', 'nullable', 'max:50'],
+        ];
+        $rules_lang = [
+            'name' => ['string', 'required', 'max:100'],
+            'title' => ['string', 'nullable', 'max:100'],
+            'subtitle' => ['string', 'nullable', 'max:100'],
+            'photo' => [new JsonFile(1, 1)],
+            'photo_m' => [new JsonFile(1, 1)],
+            'button_text' => ['string', 'nullable', 'max:50'],
         ];
         $attributes = array_merge(
                 trans('module_required::validation.attributes'),
                 trans('module_banner_carousel::validation.attributes.module_banner_carousel')
                 );
+        foreach($rules_lang as $k => $v){
+            $rules['lang.*.' . $k] = $v;
+            if(isset($attributes[$k])){
+                $attributes['lang.*.' . $k] = $attributes[$k];
+            }
+        }
 
         return $this->validate($rules, $request_data, $attributes);
     }
